771f020e2a4944826ae2e451871a1b8b42eae943
juansesu3 <juanseu3@hotmail.com>
2023-05-10 17:21:27 +0200

    security admin feactures was finished I'm going to start with admin responsive design

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,39 @@
-import clientPromise from '@/lib/mongodb'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth, { getServerSession } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
+const adminEmails = ['juan.se.suarez.ra@gmail.com'];
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    
-  ], 
-  adapter: MongoDBAdapter(clientPromise)
-})
+
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export const isAdminRequest = async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+
+}
