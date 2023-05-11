8d50e94a2cb63279e2cca3ffad6f23aba0847de4
juansesu3 <juanseu3@hotmail.com>
2023-05-11 10:00:08 +0200

    working on responsive design of e-commerce admin

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
 
   if (!session) {
     return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="bg-bgGray w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button onClick={() => signIn('google')} className="bg-white p-2 px-4 text-black rounded-lg">Login with Google</button>
         </div>
@@ -16,7 +16,7 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="bg-blue-900 min-h-screen flex">
+    <div className="bg-bgGray min-h-screen flex">
       <Nav />
       <div className="bg-white text-black flex-grow mt-2 mb-2 mr-2 rounded-lg p-4">
         {children}
