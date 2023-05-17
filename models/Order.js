117404cdb743506be694a1e5930ea0dfb15a3c40
juansesu3 <juanseu3@hotmail.com>
2023-05-17 17:27:33 +0200

    working on orders

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+import { models, model, Schema } from "mongoose";
+
+const OrderSchema = new Schema({
+    line_items:Object,
+    name:String,
+    email:String,
+    city:String,
+    postalCode:String,
+    streetAddress:String,
+    country:String,
+    paid:Boolean,
+},{
+    timestamps:true,
+}
+);
+
+export const Order = models?.Order || model('Order', OrderSchema);
---
+++ pages/orders.js
@@ -0,0 +1,56 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const OrdersPage = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Orders</h1>
+
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order.createdAt}</td>
+                <td>
+                  {order.name} {order.email}
+                  <br />
+                  {order.city} {order.postalCode}
+                  <br />
+                  {order.country}
+                  <br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data.product_data?.name} x {l.quantity}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+export default OrdersPage;
