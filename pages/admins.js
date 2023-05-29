5f16100d53de1f1d38d85f5f0427547a28388f31
juansesu3 <juanseu3@hotmail.com>
2023-05-29 16:55:40 +0200

    finished with alert, now I'm going to get admin email to show in the admin page

--- pages/admins.js
+++ pages/admins.js
@@ -1,18 +1,20 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-const AdminsPage = () => {
-    
+const AdminsPage = ({ swal }) => {
   const [email, setEmail] = useState("");
   const addAdmin = (ev) => {
     ev.preventDefault();
-    axios.post('/api/admins', {email}).then(res =>{
-        console.log(res.data);
-        alert('Created');
-        setEmail('');
-    })
-
+    axios.post("/api/admins", { email }).then((res) => {
+      console.log(res.data);
+      swal.fire({
+        title: "Admin created",
+        icon: "success",
+      });
+      setEmail("");
+    });
   };
   return (
     <Layout>
@@ -20,12 +22,13 @@ const AdminsPage = () => {
       <h2>Add new admin</h2>
       <form onSubmit={addAdmin}>
         <div className="flex gap-2">
-          <input 
-          type="text" 
-          className="mb-0" 
-          value={email}
-          onChange={ev => setEmail(ev.target.value)}
-          placeholder="google email" />
+          <input
+            type="text"
+            className="mb-0"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+            placeholder="google email"
+          />
           <button type="submit" className="btn-primary py-1 whitespace-nowrap">
             Add admin
           </button>
@@ -48,6 +51,5 @@ const AdminsPage = () => {
     </Layout>
   );
 };
-export default AdminsPage;
 
-
+export default withSwal(({ swal }, ref) => <AdminsPage swal={swal} />);
