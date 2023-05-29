bcb1307ccf8c402d6b5ea9fc799c9cefbb7d34af
juansesu3 <juanseu3@hotmail.com>
2023-05-29 17:42:30 +0200

    working on add and delete admins

--- pages/admins.js
+++ pages/admins.js
@@ -1,10 +1,14 @@
 import Layout from "@/components/Layout";
+import Spinner from "@/components/Spinner";
+import { PrettyDate } from "@/lib/date";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { withSwal } from "react-sweetalert2";
 
 const AdminsPage = ({ swal }) => {
   const [email, setEmail] = useState("");
+  const [adminEmails, setAdminEmails] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
   const addAdmin = (ev) => {
     ev.preventDefault();
     axios.post("/api/admins", { email }).then((res) => {
@@ -14,8 +18,44 @@ const AdminsPage = ({ swal }) => {
         icon: "success",
       });
       setEmail("");
+      loadAmins();
     });
   };
+  const deleteAdmin = (_id, email) => {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete admin"${email}"?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete!",
+        confirmButtonColor: "#d55",
+        reverseButtons: true,
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          axios.delete("/api/admins?_id=" + _id).then(() => {
+            swal.fire({
+              title: "Admin Deleted",
+              icon: "success",
+            });
+            loadAmins();
+          });
+        }
+      });
+  };
+
+  const loadAmins = () => {
+    setIsloading(true);
+    axios.get("api/admins").then((res) => {
+      setAdminEmails(res.data);
+      setIsloading(false);
+    });
+  };
+  useEffect(() => {
+    loadAmins();
+  }, []);
+
   return (
     <Layout>
       <h1>Admins Page</h1>
@@ -39,13 +79,39 @@ const AdminsPage = ({ swal }) => {
         <thead>
           <tr>
             <th className="text-left">Admin google email</th>
+            <th className="text-left">creation date</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>test@gmail.com</td>
-          </tr>
+          {isLoading && (
+            <tr>
+              <td colSpan={2}>
+                <div className="py-4">
+                  <Spinner />
+                </div>
+              </td>
+            </tr>
+          )}
+          {adminEmails.length > 0 &&
+            adminEmails.map((adminEmail) => (
+              <tr key={adminEmail._id}>
+                <td>{adminEmail.email}</td>
+                <td>
+                  {adminEmail.createdAt && PrettyDate(adminEmail.createdAt)}
+                </td>
+                <td>
+                  <button
+                    onClick={() =>
+                      deleteAdmin(adminEmail._id, adminEmail.email)
+                    }
+                    className="btn-red"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </Layout>
