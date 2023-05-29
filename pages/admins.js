16f4a91efe2eba71d69c92800af5d05d03393729
juansesu3 <juanseu3@hotmail.com>
2023-05-29 16:25:56 +0200

    working on admin feature now improve the UX for alerts

---
+++ pages/admins.js
@@ -0,0 +1,53 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
+
+const AdminsPage = () => {
+    
+  const [email, setEmail] = useState("");
+  const addAdmin = (ev) => {
+    ev.preventDefault();
+    axios.post('/api/admins', {email}).then(res =>{
+        console.log(res.data);
+        alert('Created');
+        setEmail('');
+    })
+
+  };
+  return (
+    <Layout>
+      <h1>Admins Page</h1>
+      <h2>Add new admin</h2>
+      <form onSubmit={addAdmin}>
+        <div className="flex gap-2">
+          <input 
+          type="text" 
+          className="mb-0" 
+          value={email}
+          onChange={ev => setEmail(ev.target.value)}
+          placeholder="google email" />
+          <button type="submit" className="btn-primary py-1 whitespace-nowrap">
+            Add admin
+          </button>
+        </div>
+      </form>
+      <h2>Existing admins</h2>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th className="text-left">Admin google email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>test@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+export default AdminsPage;
+
+
